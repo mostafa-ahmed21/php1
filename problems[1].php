5d8f5dd97169e6e8f83226ce97a05e611d7456e6
mostafa-ahmed21 <mostafaahmad.ma34@gmail.com>
2024-10-06 20:30:24 +0300

    file php1

---
+++ problems[1].php
@@ -0,0 +1,138 @@
+1. Reverse a String
+Problem:
+Write a PHP function to reverse a given string without using built-in functions.
+
+input:
+Hello World!
+Welcome Backend 31
+
+Output:
+!dlroW olleH
+13 dnekcaB emocleW
+
+<?php
+// solution...
+?>
+<!-- =============================================================== -->
+<!-- =============================================================== -->
+2. Check if a Number is Prime
+Problem:
+Write a PHP function to check whether a number is prime without using built-in functions.
+
+input:
+7
+3
+12
+
+Output:
+prime
+prime
+Not Prime
+
+<?php
+// solution...
+?>
+<!-- =============================================================== -->
+<!-- =============================================================== -->
+3. Factorial of a Number
+Problem:
+Write a PHP function to calculate the factorial of a number without using recursion or built-in functions.
+
+input:
+5
+8
+
+Output:
+120
+40320
+
+<?php
+// solution...
+?>
+<!-- =============================================================== -->
+<!-- =============================================================== -->
+4. Check if a String is a Palindrome
+Problem:
+Write a PHP function to check whether a given string is a palindrome without using built-in functions.
+
+input:
+madam
+hidden
+civic
+
+Output:
+Palindrome
+Not Palindrome
+Palindrome
+
+<?php
+// solution...
+?>
+<!-- =============================================================== -->
+<!-- =============================================================== -->
+5. Sum of Digits in a Number
+Problem:
+Write a PHP function to calculate the sum of digits of a given number without using built-in functions.
+
+input:
+12348
+9254
+
+output:
+18
+20
+
+<?php
+// solution...
+?>
+<!-- =============================================================== -->
+<!-- =============================================================== -->
+6. Check if a Number is Even or Odd
+Problem:
+Write a PHP function to check if a number is even or odd without using built-in functions.
+input:
+12
+5
+
+output:
+Even
+Odd
+
+<?php
+// solution...
+?>
+<!-- =============================================================== -->
+<!-- =============================================================== -->
+7. Find the Maximum Value in an Array
+Problem:
+Write a PHP function to find the maximum value in an array without using built-in functions.
+
+input:
+array(1, 5, 9, 3, 7)
+array(6, 2, 4, 14, 5)
+
+output:
+9
+14
+
+<?php
+// solution...
+?>
+<!-- =============================================================== -->
+<!-- =============================================================== -->
+8. Remove Duplicates from an Array
+Problem:
+Write a PHP function to remove duplicate values from an array without using built-in functions.
+
+input:
+array(1, 2, 2, 3, 4, 4, 5)
+array(6, 6, 5, 4, 12, 12, 52, 1);
+
+output:
+Array ( [0] => 1 [1] => 2 [2] => 3 [3] => 4 [4] => 5 )
+Array ( [0] => 6 [1] => 5 [2] => 4 [3] => 12 [4] => 52 [5] => 1 )
+
+
+<?php
+// solution...
+?>
